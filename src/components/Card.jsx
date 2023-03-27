a5bf1e0dbaad3f971f991139b48eb7ee68f5fa9f
saeid1994 <saeedsnaid@gmail.com>
2023-03-28 00:09:08 +0330

    fixing card page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,46 @@
 import Image from "next/image";
 
-
-export default function Card() {
-  return <section className="flex flex-col bg-green-500  p-10 gap-10">
-    <div className="shadow-md bg-white rounded-md h-[27rem]">
-      sdfds
-    </div>
-   
-  </section>;
+export default function Card(props) {
+  return (
+    <section className="flex flex-col   p-10 gap-10 ">
+      {props.data.map((i, index) => {
+        return (
+          <div
+            className="shadow-md bg-white rounded-md h-[27rem] flex flex-col"
+            key={index}
+          >
+            <Image
+              alt={i.name}
+              src={i.flags?.png}
+              width={500}
+              height={500}
+              className="h-1/ rounded-t-md"
+            />
+            <h2 className="text-xl m-6 font-bold">{i.name}</h2>
+            <span className="mx-6">
+              <h2 className="inline-block text-xl">Population: </h2>
+              <h3 className="inline-block font-thin text-lg ml-2">
+                {" "}
+                {i.population}
+              </h3>
+            </span>
+            <span className="mx-6">
+              <h2 className="inline-block text-xl">Region: </h2>
+              <h3 className="inline-block font-thin text-lg ml-2">
+                {" "}
+                {i.region}
+              </h3>
+            </span>
+            <span className="mx-6">
+              <h2 className="inline-block text-xl">Capital: </h2>
+              <h3 className="inline-block font-thin text-lg ml-2">
+                {" "}
+                {i.capital}
+              </h3>
+            </span>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,29 @@
+const fs = require("fs/promises");
+import path from "path";
 import Card from "@/components/Card";
 import Filter from "@/components/Filter";
 import Search from "@/components/Search";
-import { Fragment } from "react";
 
-export default function Home() {
+function Home(props) {
   return (
     <div className="bg-veryLightGrayLightModeBackground overflow-hidden px-3 ">
       <Search />
       <Filter />
-      <Card />
+      <Card data={props.data} />
     </div>
   );
 }
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data.json");
+  const jsonData = await fs.readFile(filePath);
+
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+export default Home;
